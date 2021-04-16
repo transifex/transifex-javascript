12a483be7ddf1a8815b3fb71e0cbee4615f152c5
Josh Peaker <3524665+JPeaker@users.noreply.github.com>
2021-04-16 12:10:37 +0100

    Add types for native exports

--- packages/native/src/index.d.ts
+++ packages/native/src/index.d.ts
@@ -1,3 +1,7 @@
 import Native = require('@transifex/native');
 export const tx : Native.tx;
 export const t : Native.t;
+export const PseudoTranslationPolicy: Native.PseudoTranslationPolicy;
+export const SourceStringPolicy: Native.SourceStringPolicy;
+export const SourceErrorPolicy: Native.SourceErrorPolicy;
+export const ThrowErrorPolicy: Native.ThrowErrorPolicy;
