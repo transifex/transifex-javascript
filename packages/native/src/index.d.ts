7b09abf8c2d6ad2bc5d2ddb349399879b5893e2d
Pablo Sanchez <pablo@transifex.com>
2022-03-30 18:44:00 +0300

    Add to TS exports Events and Utils

--- packages/native/src/index.d.ts
+++ packages/native/src/index.d.ts
@@ -7,4 +7,24 @@ declare module '@transifex/native' {
   export const ThrowErrorPolicy: any;
   export const MessageFormatRenderer: any;
   export const createNativeInstance: any;
+
+  // Events
+  export const FETCHING_TRANSLATIONS = 'FETCHING_TRANSLATIONS';
+  export const TRANSLATIONS_FETCHED = 'TRANSLATIONS_FETCHED';
+  export const TRANSLATIONS_FETCH_FAILED = 'TRANSLATIONS_FETCH_FAILED';
+  export const LOCALE_CHANGED = 'LOCALE_CHANGED';
+  export const FETCHING_LOCALES = 'FETCHING_LOCALES';
+  export const LOCALES_FETCHED = 'LOCALES_FETCHED';
+  export const LOCALES_FETCH_FAILED = 'LOCALES_FETCH_FAILED';
+  export const onEvent: any;
+  export const offEvent: any;
+  export const sendEvent: any;
+
+  // Utils
+  export const escape: any;
+  export const isString: any;
+  export const generateHashedKey: any;
+  export const generateKey: any;
+  export const isPluralized: any;
+  export const normalizeLocale: any;
 }
