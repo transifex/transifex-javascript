b23405061d65255cde8a3e49b19eaf64ad7a9348
Thanos Kolovos <codegaze@gmail.com>
2022-11-28 14:37:50 +0200

    Add support for Vue3 plugin & update CLI
    In order to support Vue3 we had to drop `vue-template-compiler`
    in favor of `@vue/compiler-sfc` for compatibility reasons. This
    way we can now support both Vue2 and Vue3.
    
    Side effects
    `vue/compiler-sfc` ast result is more cryptic than the previous
    dependency, so we might need to revisit in the future and
    use an alternative.

--- packages/cli/src/api/parsers/vue.js
+++ packages/cli/src/api/parsers/vue.js
@@ -1,11 +1,11 @@
 const _ = require('lodash');
-const vueTemplateCompiler = require('vue-template-compiler');
+const vueTemplateCompiler = require('@vue/compiler-sfc');
 const { createPayload, isPayloadValid } = require('./utils');
 const { mergePayload } = require('../merge');
 const { babelExtractPhrases } = require('./babel');
 
 /**
- * A function to traverse the AST provided by the vue-template-compiler package
+ * A function to traverse the AST provided by the vue/compiler-sfc package
  *
  * @param {*} ast
  * @param {*} visitor
@@ -14,7 +14,7 @@ function traverseVueTemplateAst(ast, visitor = {}) {
   // Use this in order to identify expressions in the AST since there is no
   // Tag to identify it with when traversing
   const VISITORS = {
-    2: 'Expression',
+    5: 'Expression',
   };
 
   function traverseArray(array, parent) {
@@ -28,10 +28,13 @@ function traverseVueTemplateAst(ast, visitor = {}) {
     if (visitor.enter) visitor.enter(node, parent);
     if (visitor[node.tag]) visitor[node.tag](node, parent);
     // Take care of expressions
-    if (visitor[VISITORS[node.type]] && node.expression) {
-      visitor[VISITORS[node.type]](node, parent);
+    if (visitor[VISITORS[node.type]] && node.content) {
+      if (node.content.loc.source) visitor[VISITORS[node.type]](node, parent);
     }
     if (node.children) traverseArray(node.children, node);
+    if (node.content) {
+      if (node.content.children) traverseArray(node.content.children, node);
+    }
     if (visitor.exit) visitor.exit(node, parent);
   }
   traverseNode(ast, null);
@@ -54,11 +57,11 @@ function vueElementVisitor(HASHES, relativeFile, options) {
   return (node) => {
     let string;
     const params = {};
-    _.each(node.attrsList, (attr) => {
+    _.each(node.props, (attr) => {
       const property = attr.name;
       if (!property || !attr.value) return;
 
-      const attrValue = attr.value;
+      const attrValue = attr.value.content;
 
       if (!attrValue) return;
 
@@ -102,25 +105,25 @@ function vueElementVisitor(HASHES, relativeFile, options) {
  */
 function extractVuePhrases(HASHES, source, relativeFile, options) {
   // Use the vue-template-compiler API to parse content
-  const vueContent = vueTemplateCompiler.parseComponent(source);
-
+  const vueContent = vueTemplateCompiler.parse(source);
   // Get the JS Content from the file and extract hashes/phrases with Babel
-  if (vueContent.script && vueContent.script.content) {
-    const script = vueContent.script.content;
+  if (vueContent.descriptor.script && vueContent.descriptor.script.content) {
+    const script = vueContent.descriptor.script.content;
     babelExtractPhrases(HASHES, script, relativeFile, options);
   }
 
   // Get the template content from the file and extract hashes/phrases with
   // custom traverse function
-  if (vueContent.template && vueContent.template.content) {
+  if (vueContent.descriptor.template && vueContent.descriptor.template.content) {
     // Compile to get the AST
-    const template = vueTemplateCompiler.compile(vueContent.template.content, {
-      preserveWhitespace: false,
+    const template = vueTemplateCompiler.compileTemplate({
+      id: 'sfc-compiler',
+      source: vueContent.descriptor.template.content,
     });
 
     traverseVueTemplateAst(template.ast, {
       Expression(node) {
-        babelExtractPhrases(HASHES, node.expression, relativeFile, options);
+        babelExtractPhrases(HASHES, node.content.loc.source, relativeFile, options);
       },
       T: vueElementVisitor(HASHES, relativeFile, options),
       UT: vueElementVisitor(HASHES, relativeFile, options),
---
+++ packages/vue3/src/index.js
@@ -0,0 +1,5 @@
+/* eslint import/prefer-default-export: 0 */
+export { default as LanguagePicker } from './components/LanguagePicker.vue';
+export { default as T } from './components/T';
+export { default as UT } from './components/UT';
+export { default as TransifexVue } from './native';
