b2a8dc4a01de32e9ff1f785d962458e604c8cdfd
J Rob Gant <rgant@alum.wpi.edu>
2023-04-15 08:20:36 -0400

    Fixt @T Decorator

--- packages/angular/projects/tx-native-angular-sdk/tests/T.decorator.spec.ts
+++ packages/angular/projects/tx-native-angular-sdk/tests/T.decorator.spec.ts
@@ -1,61 +1,55 @@
 import { Component } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { tx } from '@transifex/native';
 
 import { T } from '../src/lib/T.decorator';
 
 describe('T Decorator', () => {
-  const instanceConfig = {
-    token: 'token',
-    alias: 'alias',
-    controlled: false,
-  };
-
   @Component({
-    selector: 'test-cmp',
     template: '<div>{{ testProperty }}</div>',
-    styles: [],
   })
   class TestComponent {
     @T('not-trans-dec', { _key: 'test' }) testProperty!: string;
   }
 
   @Component({
-    selector: 'test-cmp',
     template: '<div>{{ testProperty }}</div>',
-    styles: [],
   })
   class TestWithInstanceComponent {
-    @T('not-trans-dec', { _key: 'test' }, instanceConfig) testProperty!: string;
+    @T('not-trans-dec', { _key: 'test' }, 'alias') testProperty!: string;
   }
 
-  let fixture: ComponentFixture<TestComponent>;
-
-  let fixtureWithInstance: ComponentFixture<TestWithInstanceComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({ teardown: { destroyAfterEach: false } });
+  });
 
   beforeEach(() => {
     spyOn(tx, 'translate').and.returnValue('ok-translated-dec');
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestComponent);
-    fixture.detectChanges();
-
-    fixtureWithInstance = TestBed.createComponent(TestWithInstanceComponent);
-    fixtureWithInstance.detectChanges();
-  });
-
   it('should test the decorator T', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    const component = fixture.componentInstance;
+
+    fixture.detectChanges();
+    expect(tx.translate).toHaveBeenCalledTimes(2); // Why twice?
+    expect(tx.translate).toHaveBeenCalledWith('not-trans-dec', { _key: 'test' });
+    expect(component.testProperty).toBe('ok-translated-dec');
+
     const compiled: HTMLElement = fixture.debugElement.nativeElement;
-    expect(tx.translate).toHaveBeenCalled();
     expect(compiled.innerHTML).toContain('ok-translated-dec');
   });
 
   it('should test the decorator T with an instance', () => {
+    const fixtureWithInstance = TestBed.createComponent(TestWithInstanceComponent);
+    const component = fixtureWithInstance.componentInstance;
+
+    fixtureWithInstance.detectChanges();
+    expect(tx.translate).toHaveBeenCalledTimes(2); // Why twice?
+    expect(tx.translate).toHaveBeenCalledWith('not-trans-dec', { _key: 'test' });
+    expect(component.testProperty).toBe('ok-translated-dec');
+
     const compiled: HTMLElement = fixtureWithInstance.debugElement.nativeElement;
-    expect(tx.translate).toHaveBeenCalled();
-    setTimeout(() => {
-      expect(compiled.innerHTML).toContain('ok-translated-dec');
-    }, 1000);
+    expect(compiled.innerHTML).toContain('ok-translated-dec');
   });
 });
