f3640a9cbceff3089ab4d04ac8e90f97d3357044
Nikos Vasileiou <nikosv@transifex.com>
2023-06-06 13:34:59 +0300

    Update globalObject webpack target

--- packages/jsonapi/webpack.config.js
+++ packages/jsonapi/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = [
     mode: 'production',
     entry: './src/index.js',
     output: {
+      globalObject: 'this',
       path: path.resolve(__dirname, 'dist'),
       filename: 'node.jsonapi.js',
       library: 'jsonapi',
@@ -17,6 +18,7 @@ module.exports = [
     mode: 'production',
     entry: './src/index.js',
     output: {
+      globalObject: 'this',
       path: path.resolve(__dirname, 'dist'),
       filename: 'browser.jsonapi.js',
       library: 'jsonapi',
